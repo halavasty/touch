fa5cbd8ad4bd7c7279db3fc5ab7fb86356073820
DESKTOP-AHTMLK3\sotni <sotnikov8383@gmail.com>
2022-11-15 19:17:09 +0300

    create App

---
+++ src/components/Menu/LeftMenu/LeftMenu.js
@@ -0,0 +1,35 @@
+import "./LeftMenu.css";
+
+export const LeftMenu = () => {
+  return (
+    <div className="LeftMenu">
+      <div className="Conteiner">
+        <div className="Logo"></div>
+        <div className="TextLogo">Teach.</div>
+        <div className="Avatar"></div>
+        <div className="TextAvatar">Jone Cooper</div>
+        <div className="Line"></div>
+        <div className="DashboardConteiner">
+        <div className="DashboardLogo"></div>
+        <div className="DashboardText">Dashboard</div>
+        </div>
+        <div className="ChatConteiner">
+        <div className="ChatLogo"></div>
+        <div className="ChatText">Chat</div>
+        </div >
+        <div className="MyCourseConteiner">
+        <div className="MyCourseLogo"></div>
+        <div className="MyCourseText">My Course</div></div>
+        <div className="MyAccountConteiner">
+            <div className="MyAccountLogo"></div>
+            <div className="MyAccountText">My Account</div>
+        </div>
+        <div className="LogoutConteiner">
+            <div className="LogoutLogo"></div>
+            <button className="LogoutButton">Logout</button>
+        </div>
+
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import "./Home.css";
 import { Header } from "../../components/Header";
 import { Menu } from "../../components/Menu";
+import { LeftMenu } from "../../components/Menu/LeftMenu";
 
 export const Home = () => {
   return (
     <div className="Home">
       <Header />
       <Menu />
+      <LeftMenu />
     </div>
   );
 };
